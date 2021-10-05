cc5e96c8dee5a3f1966ae2075d6bbbec8da45b7a
Nikham Sakeer P N <69519210+Nikham-Sakeer@users.noreply.github.com>
2021-10-05 19:36:50 +0530

    Create Fork.c

---
+++ Fork.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+#include<unistd.h>
+
+main()
+
+{
+
+int pid,pid1,pid2;
+
+pid=fork();
+
+if(pid==-1)
+
+{
+
+printf("ERROR IN PROCESS CREATION \n");
+
+exit(1);
+
+}
+
+if(pid!=0)
+
+{
+
+pid1=getpid();
+
+printf("\n The Parent process ID is %d\n", pid1);
+
+}
+
+else
+
+{
+
+pid2=getpid();
+
+printf("\n The Child process ID is %d\n", pid2);
+
+}
+
+}
